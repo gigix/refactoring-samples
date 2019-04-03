24db340eda5ca4d51730cdd1815b72181c5e8727
gigix <gigix1980@gmail.com>
2019-04-03 21:27:29 +0800

    Mysterious name: finally removed unused old method

--- src/main/java/org/thoughtworkers/refactoring/OrderService.java
+++ src/main/java/org/thoughtworkers/refactoring/OrderService.java
@@ -1,17 +1,6 @@
 package org.thoughtworkers.refactoring;
 
 public class OrderService {
-
-    @Deprecated
-    /**
-     * @deprecated
-     * Replaced by getPremiumDiscountAmount
-     * To be removed at May 10th
-     */
-    public double getPDA(long orderId) {
-        return getPremiumDiscountAmount(orderId);
-    }
-
     public double getPremiumDiscountAmount(long orderId) {
         Order order = Order.load(orderId);
         return order.getPremiumDiscountAmount();
